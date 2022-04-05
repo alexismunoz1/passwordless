5b090c9054a5442016f47ff2705b2678405eb21e
alexis muñoz <munozmiguelalexis@gmail.com>
2022-04-05 19:22:47 -0300

    fix add cord configurations

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { headers } from "../../next.config";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
+   headers();
    res.send({ ok: true });
 }
