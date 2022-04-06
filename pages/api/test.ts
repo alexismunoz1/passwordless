86a7a533ca90d845e9b001446af6ffc5ac2f836a
alexis muñoz <munozmiguelalexis@gmail.com>
2022-04-06 16:39:33 -0300

    add other configurations cors

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,5 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-   res.send({ ok: true });
+   const { method } = req;
+
+   if (method === "OPTIONS") {
+      return res.status(200).send("ok options");
+   }
+
+   if (method === "GET") {
+      return res.status(200).send("ok get");
+   }
+
+   if (method === "POST") {
+      return res.status(200).send("ok post");
+   }
 }
