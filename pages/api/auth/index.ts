6cdbcecc1ebaf759a181258455e9f9aba3868e94
alexis muñoz <munozmiguelalexis@gmail.com>
2022-04-06 16:03:25 -0300

    add other configurations cors

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -2,6 +2,10 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { sendCode } from "lib/controller/auth";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
+   const { method } = req;
    const result = await sendCode(req.body.email);
-   res.send(result);
+
+   if (method === "OPTIONS") {
+      return res.status(200).send(result);
+   }
 }
