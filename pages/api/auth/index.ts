a3de6690d525dbf2a92d84b3ddab534c21ddd6c6
alexis muñoz <munozmiguelalexis@gmail.com>
2022-04-05 21:10:20 -0300

    add other configurations cors

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -1,9 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { sendCode } from "lib/controller/auth";
-import { headers } from "next.config";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-   res.headersSent || headers();
    const result = await sendCode(req.body.email);
    res.send(result);
 }
--- pages/api/me.ts
+++ pages/api/me.ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { authMiddleware } from "lib/middlewares";
 import { User } from "lib/user";
-import "next.config";
 
 async function handler(req: NextApiRequest, res: NextApiResponse, token) {
    const user = new User(token.userId);
