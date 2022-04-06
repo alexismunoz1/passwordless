144870cb9f16235f96854d13cb9262d793d2ca15
alexis muñoz <munozmiguelalexis@gmail.com>
2022-04-06 17:01:42 -0300

    add other configurations cors

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -4,11 +4,11 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
    const { method } = req;
 
    if (method === "OPTIONS") {
-      return res.send({ ok: "options" });
+       res.send({ ok: "options" });
    } else if (method === "GET") {
-      return res.send({ ok: "get" });
+       res.send({ ok: "get" });
    } else if (method === "POST") {
-      return res.send({ ok: "post" });
+       res.send({ ok: "post" });
    }
-   return res.send({ ok: "other" });
+   res.send({ ok: "other" });
 }
