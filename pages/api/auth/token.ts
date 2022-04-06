56c248e680d9d2b86387fde463877f46349cafc7
alexis muñoz <munozmiguelalexis@gmail.com>
2022-04-06 18:07:34 -0300

    add other cors configurations

--- pages/api/auth/token.ts
+++ pages/api/auth/token.ts
@@ -3,6 +3,12 @@ import { generate } from "lib/jwt";
 import { Auth } from "lib/auth";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
+   const { method } = req;
+
+   if (method === "OPTIONS") {
+      return res.status(200).send("ok");
+   }
+
    const auth = await Auth.findByEmailAndCode(req.body.email, req.body.code);
    if (!auth) {
       res.status(401).send({
@@ -17,5 +23,11 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
       });
    }
    const token = generate({ userId: auth.data.userId });
-   res.send({ token });
+   if (token) {
+      res.status(200).send({ message: "hay token" });
+   } else {
+      res.status(401).send({
+         message: "no hay token",
+      });
+   }
 }
