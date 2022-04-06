2c3bcb3081b8d19e61e9af60b40968236454a16e
alexis muñoz <munozmiguelalexis@gmail.com>
2022-04-05 21:19:39 -0300

    add other configurations cors

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -3,7 +3,16 @@ import { sendCode } from "lib/controller/auth";
 import { headers } from "next.config";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
-   headers();
+   if (req.method === "POST") {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+      res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+      res.setHeader("Content-Type", "application/json");
+      res.setHeader("Access-Control-Expose-Headers", "*");
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      res.setHeader("Access-Control-Max-Age", "1728000");
+   }
+
    const result = await sendCode(req.body.email);
    res.send(result);
 }
